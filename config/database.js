52055aa9c018598bc64e94d2a8afce6d3f76d698
Kady <cvillicana@sciodev.com>
2019-05-07 15:30:58 -0500

    update database config

--- config/database.js
+++ config/database.js
@@ -1,13 +1,10 @@
 module.exports = function(){
-    switch(process.env.NODE_ENV){
-      case 'dev':
-        return{
-          'url': process.env.DB_URL, //mongodb://10.16.0.25:27017/db_lgtdev
-          options : {
-            useNewUrlParser: true,
-            user: process.env.DB_USER,  //'usr_lgtdev',
-            pass: process.env.DB_PASSWORD //'6fSLNCCkhvTK'
-          }
-        }
+  return {
+    url: process.env.DB_URL, //mongodb://10.16.0.25:27017/db_lgtdev
+    options : {
+      useNewUrlParser: true,
+      user: process.env.DB_USER,  //'usr_lgtdev',
+      pass: process.env.DB_PASSWORD //'6fSLNCCkhvTK'
     }
   }
+}
