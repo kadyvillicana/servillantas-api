f6fb3cd017ae1754a6d536452c36c4207d773012
Kady <cvillicana@sciodev.com>
2019-05-07 15:35:30 -0500

    removed comments

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 module.exports = function(){
   return {
-    url: process.env.DB_URL, //mongodb://10.16.0.25:27017/db_lgtdev
+    url: process.env.DB_URL,
     options : {
       useNewUrlParser: true,
-      user: process.env.DB_USER,  //'usr_lgtdev',
-      pass: process.env.DB_PASSWORD //'6fSLNCCkhvTK'
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD
     }
   }
 }
