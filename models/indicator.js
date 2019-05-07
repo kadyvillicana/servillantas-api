188cc7b755b99fa253373a01f088ce4921c47ee4
Claudio <cmoreno@sciodev.com>
2019-05-07 16:43:09 -0500

    created indicator and indicatorRecord models

---
+++ models/indicator.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+
+var IndicatorSchema = new mongoose.Schema({
+    item: String,
+
+    version: String,
+
+    indicatorId: String,
+
+    indicatorName: String,
+
+    definition: String,
+
+    calculateMethod: {
+        formula: String,
+        numerator: String,
+        denominator: String
+    },
+
+    measurementFrequency: {
+        annual: Boolean,
+        quarterly: Boolean,
+        monthly: Boolean
+    },
+
+    geographicBreakdown: {
+        federal: Boolean,
+        state: Boolean,
+        municipal: Boolean,
+    },
+
+    specialTreatment: String,
+    
+    indicatorWeaknesses: String,
+});
+
+module.exports = mongoose.model('Indicator', IndicatorSchema);
