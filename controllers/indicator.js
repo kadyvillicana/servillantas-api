a94e4f5e58f6634e6e0213f9d2aa8500631762df
Claudio <cmoreno@sciodev.com>
2019-05-08 09:43:25 -0500

    created get Indicators

--- controllers/indicator.js
+++ controllers/indicator.js
@@ -1,4 +1,4 @@
-var Indicator = require('../models/artist');
+var Indicator = require('../models/indicator');
 
 exports.getIndicators = (req, res, next) => {
 
@@ -13,3 +13,66 @@ exports.getIndicators = (req, res, next) => {
     });
 
 }
+
+exports.createIndicator = (req, res, next) => {
+
+    var item = req.body.item;
+    var indicatorName = req.body.indicatorName;
+    var calculateMethod = req.body.calculateMethod;
+    var measurementFrequency = req.body.measurementFrequency;
+    var geographicBreakdown = req.body.geographicBreakdown;
+    var specialTreatment = req.body.specialTreatment;
+    var indicatorWeaknesses = req.body.indicatorWeaknesses;
+
+    if(!indicatorName){
+        return res.status(400).send({error: 'You must enter a name'});
+    }
+
+    var indicator = new Indicator({
+      item: item,
+      indicatorName: indicatorName,
+      calculateMethod: calculateMethod,
+      measurementFrequency: measurementFrequency,
+      geographicBreakdown : geographicBreakdown,
+      specialTreatment: specialTreatment,
+      indicatorWeaknesses: indicatorWeaknesses,
+      indicatorId: toLowerCase(removeWhiteSpace(removeSpecialCharecters(indicatorName)))
+   });
+
+    Indicator.findOne({identifier:indicator.identifier}, (err, _indicator) => {
+      if(err){
+        return next(err);
+      }
+
+      if(_indicator){
+        return res.status(409).send({error: 'That indicator is already in our records'});
+      }
+
+      indicator.save((err, indicator) => {
+
+            if(err){
+                return next(err);
+            }
+
+            return res.status(200).json({
+              message: "Indicator successfully added!",
+              indicator: indicator
+            });
+
+        });
+
+    });
+
+}
+
+function removeSpecialCharecters(string){
+    return string.replace(/[^\w\s]/gi, '');
+  }
+  
+  function removeWhiteSpace(string){
+    return string.replace(/ /g,'');
+  }
+  
+  function toLowerCase(string){
+    return string.toLowerCase();
+  }
--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 var express = require('express')
+const indicator_controller = require('./controllers/indicator');
 
 module.exports = function(app){
 
