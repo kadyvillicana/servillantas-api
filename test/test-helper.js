b27d3d31f066cf1bb4b02e362cdf26ead5ef5df7
Misael Cabrera <misael93cr@hotmail.com>
2019-05-22 08:52:16 -0500

    Fix tests helper

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://127.0.0.1:27017/lgt_testing", { useNewUrlParser: true });
-mongoose.connection
-    .once('open', () => {})
-    .on('error', (error) => {
-        console.warn('Error : ', error);
-    });
+// Connect to db before running the tests
+before((done) => {
+    mongoose.connect("mongodb://127.0.0.1:27017/lgt_testing", { useNewUrlParser: true });
+    mongoose.connection
+        .once('open', () => {})
+        .on('error', (error) => {
+            console.warn('Error : ', error);
+        });
 
-// Runs before each test
+    done();
+});
+
+// Drop collections after each test
 beforeEach((done) => {
-
     for (var collection in mongoose.connection.collections) {
-        mongoose.connection.collections[collection].drop();
+        mongoose.connection.collections[collection].drop()
+            .catch(() => console.warn('Error dropping collection. It may not exist.'));
     }
 
     done();
 });
 
+// Disconnect from db after all tests have run
 after((done) => {
     mongoose.disconnect();
     done();
