02526cc93f5c8a49e63f5648d4200809fc8bfc64
Claudio <cmoreno@sciodev.com>
2019-06-27 15:13:26 -0500

    removed unused properties from indicator record model

--- models/indicatorRecord.js
+++ models/indicatorRecord.js
@@ -22,47 +22,6 @@ const indicatorRecordSchema = new mongoose.Schema(
       type: String,
       required: true
     },
-    condemnatory:{
-      type: Number,
-      default: 0
-    },
-    absolut:{
-      type: Number,
-      default: 0
-    },
-    condemnedPeople:{
-      type: Number,
-      default: 0
-    },
-    victimNumber:{
-      type: Number,
-      default: 0
-    },
-    gender: {
-      type: {
-        male: {
-          type: Number
-        },
-        female: {
-          type: Number
-        }
-      },
-      default: { male: 0, female: 0 }
-    },
-    investigationFolder: {
-      type: {
-        complaint: {
-          type: Number
-        },
-        judicialHearing: {
-          type: Number
-        },
-        otherReasons: {
-          type: Number
-        }
-      },
-      default: { complaint: 0, judicialHearing: 0, otherReasons: 0 }
-    }
   },
   {
     timestamps: true
